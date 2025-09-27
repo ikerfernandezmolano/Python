a8a75c9d51d31c03223cc596b8205506d918b7e8
iker. <ikerfernandezmolano@gmail.com>
2025-09-27 16:31:11 +0200

    inter

--- 20_TKinter/01_Ventana.py
+++ 20_TKinter/01_Ventana.py
@@ -2,20 +2,44 @@
 # Módulo para crear interfaces gráficas de usuario
 
 from tkinter import *
+import os.path
 
-# Crear la ventana raíz
-ventana = Tk()
+class Programa:
 
-# Icono de la ventana
-# ventana.iconbitmap("./imagenes/logo.ico") # Funciona en windows
-# icono = PhotoImage(file="./imagenes/logo.png")  # usa .png en lugar de .ico
-# ventana.iconphoto(True, icono)
+    def __init__(self):
+        self.title = "Interfaz Gráfica"
+        self.icon = os.path.abspath("./imagenes/logo.ico")
+        self.icon_alt = os.path.abspath("./20_TKinter/imagenes/logo.ico")
+        self.size = "750x450"
+        self.resizable = False
+        # Crear la ventana raíz
+        self.ventana = Tk()
 
-# Cambio en el tamaño de la ventana
-ventana.geometry("750x450")
+    def cargar(self):
+        # Mostrar texto en el programa
+        self.ventana.title(self.title)
+        # Icono de la ventana
+        try:
+            self.ventana.iconbitmap(self.icon)
+            texto = Label(self.ventana, text=self.icon)
+        except:
+            self.ventana.iconbitmap(self.icon_alt)
+            texto = Label(self.ventana, text=self.icon_alt)
+        texto.pack()
+        # Cambio en el tamaño de la ventana
+        self.ventana.geometry(self.size)
+        # Bloquear el tamaño de la ventana
+        self.ventana.resizable(self.resizable, self.resizable)
 
-# Bloquear el tamaño de la ventana
-ventana.resizable(1,1)
+    def addText(self, pText):
+        texto = Label(self.ventana, text=pText)
+        texto.pack()
 
-# Arrancar y mostrar la ventana hasta que se cierre
-ventana.mainloop()
+    def show(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
+
+programa = Programa()
+programa.cargar()
+programa.addText("Hola Mundo")  
+programa.show()
